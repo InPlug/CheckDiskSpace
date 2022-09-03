0811c23077da349a7877192f8ec9e262fed27b82
Erik Nagel <erik@reallyhuman.net>
2022-09-03 19:39:09 +0200

    ComplexCheckDiskSpaceReturnObject: ToString ueberarbeitet.

--- CheckDiskSpace/ComplexCheckDiskSpaceReturnObject.cs
+++ CheckDiskSpace/ComplexCheckDiskSpaceReturnObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace CheckDiskSpace
@@ -57,6 +58,92 @@ namespace CheckDiskSpace
         /// Anzahl Ping-Versuche, bevor ein Fehler erzeugt wird.
         /// </summary>
         public int Retries { get; set; }
+
+        /// <summary>
+        /// Standard Konstruktor.
+        /// </summary>
+        public ComplexCheckDiskSpaceReturnObject() { }
+
+        /// <summary>
+        /// Deserialisierungs-Konstruktor.
+        /// </summary>
+        /// <param name="info">Property-Container.</param>
+        /// <param name="context">Übertragungs-Kontext.</param>
+        protected ComplexCheckDiskSpaceReturnObject(SerializationInfo info, StreamingContext context)
+        {
+            this.Server = info.GetString("Server");
+            this.DriveLetter = info.GetString("DriveLetter");
+            this.CriticalFreeMBytesAvailable = (long)info.GetValue("CriticalFreeMBytesAvailable", typeof(long));
+            this.TotalNumberOfMBytes = (long)info.GetValue("TotalNumberOfMBytes", typeof(long));
+            this.FreeMBytesAvailable = (long)info.GetValue("FreeMBytesAvailable", typeof(long));
+            this.SQLServerAccess = (bool)info.GetValue("SQLServerAccess", typeof(bool));
+            this.Timeout = (int)info.GetValue("Timeout", typeof(int));
+            this.Retries = (int)info.GetValue("Retries", typeof(int));
+        }
+
+        /// <summary>
+        /// Serialisierungs-Hilfsroutine: holt die Objekt-Properties in den Property-Container.
+        /// </summary>
+        /// <param name="info">Property-Container.</param>
+        /// <param name="context">Serialisierungs-Kontext.</param>
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Server", this.Server);
+            info.AddValue("DriveLetter", this.DriveLetter);
+            info.AddValue("CriticalFreeMBytesAvailable", this.CriticalFreeMBytesAvailable);
+            info.AddValue("TotalNumberOfMBytes", this.TotalNumberOfMBytes);
+            info.AddValue("FreeMBytesAvailable", this.FreeMBytesAvailable);
+            info.AddValue("SQLServerAccess", this.SQLServerAccess);
+            info.AddValue("Timeout", this.Timeout);
+            info.AddValue("Retries", this.Retries);
+        }
+
+        /// <summary>
+        /// Überschriebene ToString()-Methode - stellt öffentliche Properties
+        /// als einen (mehrzeiligen) aufbereiteten String zur Verfügung.
+        /// </summary>
+        /// <returns>Alle öffentlichen Properties als ein String aufbereitet.</returns>
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            string serverDrive = String.IsNullOrEmpty(Server) ? DriveLetter : Server + (String.IsNullOrEmpty(DriveLetter) ? "" : @"//" + DriveLetter);
+            str.Append(String.Format("{0}: gesamt: {1} MB, frei: {2} MB, Minimum: {3} MB",
+                       serverDrive, TotalNumberOfMBytes, FreeMBytesAvailable, CriticalFreeMBytesAvailable));
+            return str.ToString();
+        }
+
+        /// <summary>
+        /// Vergleicht dieses Result mit einem übergebenen Result nach Inhalt.
+        /// Der Timestamp wird bewusst nicht in den Vergleich einbezogen.
+        /// </summary>
+        /// <param name="obj">Das zu vergleichende Result.</param>
+        /// <returns>True, wenn das übergebene Result inhaltlich (ohne Timestamp) gleich diesem Result ist.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+            if (Object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (this.ToString() != obj.ToString())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Erzeugt einen eindeutigen Hashcode für dieses Result.
+        /// Der Timestamp wird bewusst nicht in den Vergleich einbezogen.
+        /// </summary>
+        /// <returns>Hashcode (int).</returns>
+        public override int GetHashCode()
+        {
+            return (this.ToString()).GetHashCode();
+        }
 
     }
 }
--- CheckDiskSpaceDemo/Program.cs
+++ CheckDiskSpaceDemo/Program.cs
@@ -16,14 +16,7 @@ namespace CheckDiskSpaceDemo
             //res = checkDiskSpace.Run(@"Localhost\d$|1000|100|1|true", new TreeParameters("MainTree", null), null);
             res = checkDiskSpace.Run(@"D|20184|100|3", new TreeParameters("MainTree", null), null);
             //res = checkDiskSpace.Run(@"\\DataServer\c$|100000|100|3", new TreeParameters("MainTree", null), null);
-            Console.WriteLine("{0}\\{1}: gesamt: {2} MB, frei: {3} MB, Minimum: {4} MB, Ergebnis: {5}",
-              (checkDiskSpace.ReturnObject as ComplexCheckDiskSpaceReturnObject).Server,
-              (checkDiskSpace.ReturnObject as ComplexCheckDiskSpaceReturnObject).DriveLetter,
-              (checkDiskSpace.ReturnObject as ComplexCheckDiskSpaceReturnObject).TotalNumberOfMBytes,
-              (checkDiskSpace.ReturnObject as ComplexCheckDiskSpaceReturnObject).FreeMBytesAvailable,
-              (checkDiskSpace.ReturnObject as ComplexCheckDiskSpaceReturnObject).CriticalFreeMBytesAvailable,
-              res == null ? "unklar" : res == true ? "ok." : "nicht ok."
-            );
+            Console.WriteLine(res.ToString() + " - " + checkDiskSpace.ReturnObject.ToString());
             Console.ReadLine();
         }
 
